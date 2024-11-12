50ac7c1f34e4d2a85f29a59c4f8490a30fa17807
Quoc Trung <0306221087@caothang.edu.vn>
2024-11-12 23:32:46 +0700

    HomeviewModel

--- app/src/main/java/com/example/chatsimple_firebase/AccountViewModel.kt
+++ app/src/main/java/com/example/chatsimple_firebase/AccountViewModel.kt
@@ -4,12 +4,46 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
 
 class AccountViewModel:ViewModel(){
     var state by mutableStateOf(AccountState())
         private set
     fun addUser(){
-
+        FirebaseAuth.getInstance()
+            .createUserWithEmailAndPassword(state.email,state.password)
+            .addOnCompleteListener{
+                if(!it.isSuccessful){
+                    state = state.copy(success = false)
+                }
+                else{
+                    val user = User(
+                        state.email,
+                        state.fullname,
+                        state.password
+                    )
+                    Firebase.firestore.collection("users").add(user)
+                    state = state.copy(success = true)
+                }
+            }
+    }
+    fun SignIn(){
+        FirebaseAuth.getInstance()
+            .signInWithEmailAndPassword(state.email,state.password)
+            .addOnCompleteListener{
+                state = state.copy(success = it.isSuccessful)
+            }
+    }
+    fun onChangeEmail(newvalue:String){
+        state = state.copy(email = newvalue)
+    }
+    fun onChangeFullname(newvalue:String){
+        state = state.copy(fullname = newvalue)
+    }
+    fun onChangePassword(newvalue:String){
+        state = state.copy(password = newvalue)
     }
 }
 
---
+++ app/src/main/java/com/example/chatsimple_firebase/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.chatsimple_firebase
+
+import androidx.lifecycle.ViewModel
+
+class  HomeViewModel: ViewModel(){
+    var state by mutab
+}
